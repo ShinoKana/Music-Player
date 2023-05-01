3301bf00accae4dc90acfa60d95a9420fc3a3294
NING Chenyu <108135450+ncy5239@users.noreply.github.com>
2023-05-01 11:41:56 +0800

    更改一些typo

--- UI/pages/playerPage.py
+++ UI/pages/playerPage.py
@@ -4,30 +4,55 @@ from typing import Union
 from PySide2.QtWidgets import QFrame, QLayout, QLabel, QVBoxLayout, QHBoxLayout
 from PySide2.QtGui import QPixmap
 from components.AppScrollBox import AppScrollBox
+from Core import appManager
+
+
 import os
 
 class PlayerPage(AppPage):
+    
     def __init__(self, appWindow, parent: Union[QFrame, QLayout] = None):
         super().__init__(appWindow=appWindow, parent=parent, titleText=AutoTranslateWord("Play"))
-
-        #Create images and Lyrics scroll box
+        print("Initial")
         #TODO: Because the color of the scroll box is different
         #TODO: I deliberately set to white, which may lead to the dark mode display strange
-        self.imageLayout = QVBoxLayout()
-        self.imageLabel = QLabel()
-        self.imageLayout.addWidget(self.imageLabel)
         self.pageLayout = QHBoxLayout()
-        self.pageLayout.addLayout(self.imageLayout)
-        self.scrollBox = AppScrollBox(titleText="Lyrics")
-        self.scrollBox.setStyleSheet("QLabel { padding: 5px; }")
-        self.pageLayout.addWidget(self.scrollBox)
-        self.scrollBox.viewport().setStyleSheet("background-color: #FFFFFF;")
+        self.setLayout(self.pageLayout)
+        self.labels = []
+        self.switchingIn = False
+        self.isDark = appManager.config.isDarkTheme()
 
-
+        
+        
 
     def onSwitchIn(self):
-        #TODO:Here the file address is found based on the absolute path, and the file name is also fixed
-        #TODO:Need to get the name of the song and find the corresponding file
+        print("onSwitchIn")
+            #TODO:Here the file address is found based on the absolute path, and the file name is also fixed
+            #TODO:Need to get the name of the song and find the corresponding file
+        
+        self.switchingIn = True
+        if not hasattr(self, 'imageLayout'):
+            # Create the image layout if it doesn't exist
+            self.imageLayout = QVBoxLayout()
+            self.imageLabel = QLabel()
+            self.imageLayout.addWidget(self.imageLabel)
+            self.pageLayout.addLayout(self.imageLayout)
+
+        if not hasattr(self, 'scrollBox'):
+            # Create the scroll box if it doesn't exist
+            self.scrollBox = AppScrollBox(titleText="Lyrics")
+            #self.scrollBox.setStyleSheet("QLabel { padding: 5px; }")
+            self.pageLayout.addWidget(self.scrollBox)
+            #self.scrollBox.viewport().setStyleSheet("background-color: #FFFFFF;")
+
+        app_music_box = self.appWindow.musicBox
+        app_music_box.music_info_signal.connect(self.print_music_info)
+
+        self.scrollBox.removeAllComponents()
+        self.lyrics = []
+        self.labels = []
+        self.timeline = []
+
         self.imagePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "image1.png")
         file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "text_file.txt")
         
@@ -39,29 +64,46 @@ class PlayerPage(AppPage):
         #Lyrics(Japanese) with timeline is in 'text_file.txt' 
         with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
+            self.lyrics = lines
             for line in lines:
                 label = QLabel(line.strip().replace("\n",""))
                 label.setFixedHeight(30)
+                if self.isDark:
+                    label.setStyleSheet("color: white;")
+                self.labels.append(label)
                 self.scrollBox.addComponent(label)
 
             #The following codes calculates the time interval between each two lines of text, 
             # i.e. the time each line of text should be displayed
             #The results are stored in the list "delay"
-            delay = []
-            for i in range(0,len(lines)):
-                if(i == 0):
-                    time = lines[0].split(']')[0][1:]
-                    time = int(time.split(':')[0])*60000 + int(time.split(':')[1].split('.')[0])*1000+ int(time.split(':')[1].split('.')[1])
-                    delay.append(time)
-                else:
-                    time = [lines[i].split(']')[0], lines[i-1].split(']')[0]]
-                    for j in range(2):
-                        time[j] = time[j][1:]
-                        time[j] = int(time[j].split(':')[0])*6000 + int(time[j].split(':')[1].split('.')[0])*1000+ int(time[j].split(':')[1].split('.')[1])
-                    delay.append(abs(time[0]-time[1]))
 
-                    
+            for i in range(0,len(lines)):
+                time = lines[i].split(']')[0][1:]
+                time = int(time.split(':')[0])*60 + int(time.split(':')[1].split('.')[0])
+                self.timeline.append(time)
+
+
+    # Add this method to the PlayerPage class####################################
+    def print_music_info(self, title, position):
+        if self.switchingIn:
+            for i in range(0, len(self.timeline)-1):
+                if self.timeline[0] > position:
+                    for label in self.labels:
+                        label.setStyleSheet("")
+                        if self.isDark:
+                            label.setStyleSheet("color: white;")
+                        label.setFixedHeight(30)
+                    self.labels[0].setStyleSheet("font-weight: bold; color: red;")
+                elif self.timeline[i] < position < self.timeline[i+1]:
+                    for label in self.labels:
+                        label.setStyleSheet("")
+                        if self.isDark:
+                            label.setStyleSheet("color: white;")
+                        label.setFixedHeight(30)
+                    self.labels[i].setStyleSheet("font-weight: bold; color: red;")
+
             
     def onSwitchOut(self):
-        pass;
-
+        print("onSwitchOut")
+        self.switchingIn = False
+        pass
